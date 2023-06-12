78b02355b4d536ea1ba015218b708f778cd9593d
Patience Muru <murup@my.lanecc.edu>
2023-06-11 23:16:49 -0700

    finished

--- src/script.js
+++ src/script.js
@@ -1,60 +1,57 @@
-// Get references to the HTML elements
-const movieInput = document.getElementById('movieInput');
-const searchButton = document.getElementById('searchButton');
-const movieTitle = document.getElementById('movieTitle');
-const movieYear = document.getElementById('movieYear');
-const movieGenre = document.getElementById('movieGenre');
-const moviePlot = document.getElementById('moviePlot');
-const resultContainer = document.getElementById('resultContainer');
-
-// Function to fetch movie data from an API
-async function fetchMovieData(movieTitle) {
-  const apiKey = '173a1549';
-  const url = `http://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apiKey=${apiKey}`;
+class MovieApp {
+    constructor() {
+      this.movieInput = document.getElementById('movieInput');
+      this.searchButton = document.getElementById('searchButton');
+      this.movieTitle = document.getElementById('movieTitle');
+      this.movieYear = document.getElementById('movieYear');
+      this.movieGenre = document.getElementById('movieGenre');
+      this.moviePlot = document.getElementById('moviePlot');
+      this.moviePoster = document.getElementById('moviePoster');
+      this.resultContainer = document.getElementById('resultContainer');
+      
+      this.searchButton.addEventListener('click', this.searchMovie.bind(this));
+    }
     
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching movie data:', error);
-    return null;
-  }
-}
-
-function displayMovieInformation(movie) {
-    console.log(movie); // Log the movie object for debugging purposes
+    async fetchMovieData(movieTitle) {
+      const apiKey = '173a1549';
+      const url = `http://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apiKey=${apiKey}`;
   
-    if (movie && movie.Response === "True") {
-      movieTitle.textContent = movie.Title;
-      movieYear.textContent = 'Year: ' + movie.Year;
-      movieGenre.textContent = 'Genre: ' + movie.Genre;
-      moviePlot.textContent = 'Plot: ' + movie.Plot;
-  
-      // Set the movie poster
-      const moviePoster = document.getElementById('moviePoster');
-      moviePoster.src = movie.Poster;
-  
-      resultContainer.style.display = 'block';
-    } else {
-      movieTitle.textContent = 'Movie not found.';
-      movieYear.textContent = '';
-      movieGenre.textContent = '';
-      moviePlot.textContent = '';
-  
-      // Reset the movie poster
-      const moviePoster = document.getElementById('moviePoster');
-      moviePoster.src = '';
-  
-      resultContainer.style.display = 'none';
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+        return null;
+      }
+    }
+    
+    displayMovieInformation(movie) {
+      if (movie && movie.Response === 'True') {
+        this.movieTitle.textContent = movie.Title;
+        this.movieYear.textContent = 'Year: ' + movie.Year;
+        this.movieGenre.textContent = 'Genre: ' + movie.Genre;
+        this.moviePlot.textContent = 'Plot: ' + movie.Plot;
+        this.moviePoster.src = movie.Poster;
+        this.resultContainer.style.display = 'block';
+      } else {
+        this.movieTitle.textContent = 'Movie not found.';
+        this.movieYear.textContent = '';
+        this.movieGenre.textContent = '';
+        this.moviePlot.textContent = '';
+        this.moviePoster.src = '';
+        this.resultContainer.style.display = 'none';
+      }
+    }
+    
+    async searchMovie() {
+      const title = this.movieInput.value.trim();
+      if (title !== '') {
+        const movieData = await this.fetchMovieData(title);
+        this.displayMovieInformation(movieData);
+      }
     }
   }
   
-// Event listener for the search button
-searchButton.addEventListener('click', async () => {
-  const title = movieInput.value.trim();
-  if (title !== '') {
-    const movieData = await fetchMovieData(title);
-    displayMovieInformation(movieData);
-  }
-});
+  const app = new MovieApp();
+  
